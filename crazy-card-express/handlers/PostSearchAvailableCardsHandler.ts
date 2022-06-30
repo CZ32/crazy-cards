dad1c058d8bdb2a585c53260dfffce9be892eace
Cengizhan Ziyaeddin <66175507+CZ32@users.noreply.github.com>
2022-06-30 14:42:35 +0100

    handler returns unfiltered data

--- crazy-card-express/handlers/PostSearchAvailableCardsHandler.ts
+++ crazy-card-express/handlers/PostSearchAvailableCardsHandler.ts
@@ -1,13 +1,3 @@
-export async function PostSearchAvailableCardsHandler () {
-    
-}
-
-const filterCardsByRequirment = (userData: PostSearchAvailableCardsRequestBody): Cards[] => {
-    //Can user have liquid card
-    //! income > 16k 
-    //! isGoodEmploymentStatus === 'partTime' || 'full-time' || 'student'
-    //Can user have Student card 
-    //! Is user a student? 
-    //Any user can have anywhere card
-    //!anyone
+export async function PostSearchAvailableCardsHandler (req: Express.Request, res: Express.Response): Promise<PostSearchAvailableCardsResponseBody> {
+    return { cards: cardData }
 }
---
+++ crazy-card-express/helpers/index.ts
@@ -0,0 +1,10 @@
+export const filterCardsByRequirement = (userData: PostSearchAvailableCardsRequestBody): Card[] => {
+    //Can user have liquid card
+    //! income > 16k 
+    //! isGoodEmploymentStatus === 'partTime' || 'full-time' || 'student'
+    //Can user have Student card 
+    //! Is user a student? 
+    //Any user can have anywhere card
+    //!anyone
+    return cardData
+}
