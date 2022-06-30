f95b05251d6964ba0ab67ca15d6220837c5fc7ec
Cengizhan Ziyaeddin <66175507+CZ32@users.noreply.github.com>
2022-06-30 14:32:06 +0100

    Created mock code for filter logic

--- crazy-card-express/handlers/PostSearchAvailableCardsHandler.ts
+++ crazy-card-express/handlers/PostSearchAvailableCardsHandler.ts
@@ -2,6 +2,12 @@ export async function PostSearchAvailableCardsHandler () {
     
 }
 
-const filterCardsByRequirment = (userData: PostSearchAvailableCardsRequestBody) => {
-    
+const filterCardsByRequirment = (userData: PostSearchAvailableCardsRequestBody): Cards[] => {
+    //Can user have liquid card
+    //! income > 16k 
+    //! isGoodEmploymentStatus === 'partTime' || 'full-time' || 'student'
+    //Can user have Student card 
+    //! Is user a student? 
+    //Any user can have anywhere card
+    //!anyone
 }
